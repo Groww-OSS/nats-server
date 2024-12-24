54059802977097501c75b3644733add8643061a5
Aditya Agarwal <aditya.agarwal@groww.in>
2024-12-24 16:22:29 +0530

    Added More Test

--- server/subject_transform_test.go
+++ server/subject_transform_test.go
@@ -232,4 +232,5 @@ func TestSubjectTransforms(t *testing.T) {
 	shouldMatch("*", "{{left(1,6)}}", "1234", "1234")
 	shouldMatch("foo.*", "bar.{{custom(1)}}", "foo.hello","bar.customhello")
 	shouldMatch("foo.*.*.bar", "bar.{{custom(2)}}.{{custom(1)}}", "foo.1.2.bar","bar.custom2.custom1")
+	shouldMatch("user.details.*", "user.details.{{custom(1)}}", "user.details.11","user.details.custom11")
 }
